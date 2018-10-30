3b55b1c362b81b0ab04f84cd27b1e3931ecf8307
strivema <marui@yinker.com>
2018-10-30 16:54:50 +0800

    leetcode算法刷题---删除链表的节点

---
+++ src/xiaodai/algorithm/easy/list/ListNode.java
@@ -0,0 +1,14 @@
+package xiaodai.algorithm.easy.list;
+
+/**
+ * @athour Marie
+ * @date 2018/10/30 4:44 PM
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+    }
+
+}
