1631aacc81467e575a95648bd229c34f0707bc50
strivema <marui@yinker.com>
2018-10-31 23:40:19 +0800

    leetcode算法刷题---树---层次遍历 -----最大深度 -----对称二叉树 -----将有序数组转化为二叉搜索树

--- src/xiaodai/algorithm/easy/tree/SortArrayToBST.java
+++ src/xiaodai/algorithm/easy/tree/SortArrayToBST.java
@@ -5,5 +5,24 @@ package xiaodai.algorithm.easy.tree;
  * @date 2018/10/31 8:24 PM
  **/
 public class SortArrayToBST {
+    public TreeNode sortArrayTpBST(int []nums){
+        if(nums==null||nums.length<=0){
+            return null;
+        }
+        return build(nums,0,nums.length-1);
+
+    }
+    public TreeNode build(int []nums,int start,int end){
+        if (start<end){
+            int mid = (start+end)/2;
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = build(nums,start,mid-1);
+            root.right = build(nums,mid+1,end);
+            return root;
+
+        }else
+
+            return null;
+    }
 
 }
