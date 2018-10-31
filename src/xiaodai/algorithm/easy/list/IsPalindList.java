79c51f189423b61d9a7d03d9e71308e6f756b4af
strivema <marui@yinker.com>
2018-10-31 13:57:23 +0800

    leetcode算法刷题---判断回文链表 使用栈作为存储结构

--- src/xiaodai/algorithm/easy/list/IsPalindList.java
+++ src/xiaodai/algorithm/easy/list/IsPalindList.java
@@ -36,5 +36,30 @@ public class IsPalindList {
         }
         return true;
      }
+     public static boolean isPalindList(ListNode head){
+        if(head==null||head.next==null){
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Stack<ListNode> stack = new Stack<>();
+        while(slow!=null){
+            stack.push(slow);
+            slow = slow.next;
+         }
+         slow =head;
+        while (!stack.isEmpty()){
+            if(slow.val!=stack.pop().val){
+                return false;
+            }
+            slow = slow.next;
+        }
+        return true;
+
+     }
 
 }
