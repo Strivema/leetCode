96d1659c1abd947b422c19d8e2607e2c6e0ca472
strivema <marui@yinker.com>
2018-11-04 16:00:52 +0800

    leetcode算法刷题---二叉树生成

---
+++ src/xiaodai/algorithm/simple/tree/BulidTree.java
@@ -0,0 +1,41 @@
+package xiaodai.algorithm.simple.tree;
+
+import xiaodai.algorithm.easy.tree.TreeNode;
+
+/**
+ * @athour Marie
+ * @date 2018/11/3 3:15 PM
+ **/
+public class BulidTree {
+    public TreeNode bulidTree(int []preorder,int[] inorder){
+        if(preorder==null||inorder==null||preorder.length==0){
+            return null;
+        }
+        return bulid(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
+
+    }
+    public static TreeNode bulid(int []preorder,int prest,int prend,int[]inorder,int inst,int inend){
+        int rootval = preorder[prest];
+        TreeNode root = new TreeNode(rootval);
+        if(prend ==prest){
+            return root;
+        }
+        int inroot = inst;
+        while (inorder[inroot]!=rootval&&inroot<inend){
+            inroot++;
+        }
+        // 左子树的长度
+        int length = inroot -inst;
+        int pre  =prest+length;
+
+        if(length>0){
+            root.left = bulid(preorder,prest+1,pre,inorder,inst,inst);
+        }
+        if(length<prend-prest){
+            root.right = bulid(preorder,pre+1,prend,inorder,inroot+1,inst);
+        }
+        return root;
+
+    }
+
+}
