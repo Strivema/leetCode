4073d4e4f12617542cc12e97b03c9d6ace651dca
strivema <marui@yinker.com>
2018-11-28 20:35:19 +0800

    leetcode 不同路径

--- src/xiaodai/algorithm/simple/dynamicPlanning/UniquePaths.java
+++ src/xiaodai/algorithm/simple/dynamicPlanning/UniquePaths.java
@@ -1,7 +1,6 @@
 package xiaodai.algorithm.simple.dynamicPlanning;
 
 
-
 /**
  * @athour Marie
  * @date 2018/11/28 7:59 PM
@@ -16,17 +15,17 @@ public class UniquePaths {
         for (int i = 1; i < n; i++) {
             dp[0][i] = 1;
         }
-        for(int i =1;i<m;i++){
-            for (int j = 1;j<n;j++){
-                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        return dp[m-1][n-1];
+        return dp[m - 1][n - 1];
 
     }
 
     public static void main(String[] args) {
-        System.out.println(new UniquePaths().uniquePaths(7,3));
+        System.out.println(new UniquePaths().uniquePaths(7, 3));
     }
 
 }
